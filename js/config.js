66da3a459dc313e8bb73f2f50dc1e46f21400cba
ekap6230 <eyal.kapon@niceactimize.com>
2021-02-22 12:28:30 +0200

    adding auth

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_D6Hoct2pf', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7fgfmr2m9qtpmmv1ud9f6joufc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
